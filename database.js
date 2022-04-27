511906f7e94dceb46e2a72a387f4fb941ef488cb
Lahiru Wijesekara <opencourse5tutor@codl.lk>
2022-04-27 14:46:00 +0530

    first commit

---
+++ database.js
@@ -0,0 +1,39 @@
+var sqlite3 = require('sqlite3').verbose()
+var md5 = require('md5')
+
+const DBSOURCE = "db.sqlite"
+
+
+let db = new sqlite3.Database(DBSOURCE, (err) => {
+    if (err) {
+        // Cannot open database
+        console.error(err.message)
+        throw err
+    } else {
+        console.log('Connected to the SQlite database.')
+        db.run(`CREATE TABLE products (
+            id INTEGER PRIMARY KEY AUTOINCREMENT,
+            productName text, 
+            description text,
+            category text,
+            brand text,
+            expiredDate text,
+            manufacturedDate text,
+            batchNumber INTEGER,
+            unitPrice INTEGER,
+            quantity INTEGER,
+            createdDate text
+            )`, (err) => {
+            if (err) {
+                // Table already created
+            } else {
+                // Table just created, creating some rows
+                var insert = 'INSERT INTO products (productName, description, category, brand, expiredDate, manufacturedDate, batchNumber, unitPrice, quantity, createdDate) VALUES (?,?,?,?,?,?,?,?,?,?)'
+                db.run(insert, ["White Basmathi Rice", "White Basmathi Rice imported from Pakistan. High-quality rice with extra fragrance. Organically grown.", "Rice", "CIC", "2023.05.04", "2022.02.20", 324567, , 1020, 200, "2022.02.24"])
+            }
+        })
+
+    }
+})
+
+module.exports = db
